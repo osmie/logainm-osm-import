0b9fdd6468a2b9deaaf7b771a45f2d13d546c939
Rory McCann <rory@technomancy.org>
2015-02-22 18:55:34 +0100

    code

--- match-up.py
+++ match-up.py
@@ -63,6 +63,25 @@ def single_exact_name_and_optional_name_ga_match_and_inside(townland, logainms):
     else:
         return None
 
+def name_and_optional_name_ga_match_very_near(townland, logainms):
+    name = townland['properties'].get('NAME')
+    name_ga = townland['properties'].get('NAME:GA')
+
+    if name_ga:
+        same_name = [l for l in logainms if l['name_en'] == name and l['name_ga'] == name_ga]
+    else:
+        same_name = [l for l in logainms if l['name_en'] == name]
+
+    dists = [dist(townland['geom_3857'], l['point_3857']) for l in same_name]
+    logger.info(dists)
+    if sum(1 for x in dist if x == 0) == 1 and len(x for x in dist is x > 20) == len(dists) - 1:
+        for dist, l in zip(dists, same_name):
+            if dist == 0:
+                return l
+
+    return None
+
+
 def get_existing_osm_tags(xml_el):
     """Given a XML element for an object, return (as dict) the current OSM tags"""
     return {el.get('k'): el.get('v') for el in xml_el.findall("tag")}
@@ -77,17 +96,19 @@ def logainm_tags(xml_el, logainm_data):
         'logainm:url': 'http://www.logainm.ie/en/{}'.format(logainm_data['logainm_id'])
     }
 
-    if 'name:ga' not in tags:
-        new_tags['name:ga'] = logainm_data['name_ga']
-    else:
-        if tags['name:ga'] != logainm_data['name_ga']:
-            new_tags['offical_name:ga'] = logainm_data['name_ga']
+    if logainm_data['name_ga'] not in ["", None]:
+        if 'name:ga' not in tags:
+            new_tags['name:ga'] = logainm_data['name_ga']
+        else:
+            if tags['name:ga'] != logainm_data['name_ga']:
+                new_tags['offical_name:ga'] = logainm_data['name_ga']
 
-    if 'name:en' not in tags:
-        new_tags['name:en'] = logainm_data['name_en']
-    else:
-        if tags['name:en'] != logainm_data['name_en']:
-            new_tags['offical_name:en'] = logainm_data['name_en']
+    if logainm_data['name_en'] not in ["", None]:
+        if 'name:en' not in tags:
+            new_tags['name:en'] = logainm_data['name_en']
+        else:
+            if tags['name:en'] != logainm_data['name_en']:
+                new_tags['offical_name:en'] = logainm_data['name_en']
 
     return new_tags
 
@@ -116,7 +137,7 @@ def main():
     logger.setLevel(logging.DEBUG)
 
     with printer("reading in OSM shapefile"):
-        with fiona.open("townlands/townlands.shp") as src:
+        with fiona.open("townlands/townlands.shp", encoding="utf8") as src:
             townlands = list(src)
 
         # add geom
@@ -136,8 +157,11 @@ def main():
         for l in logainms:
             l['point'] = Point((float(l['lon']), float(l['lat'])))
             l['point_3857'] = transform(latlon_to_3857, l['point'])
+            l['name_ga'] = l['name_ga'].decode("utf8")
 
         logger.info("Loaded %d Logainm rows", len(logainms))
+
+    #import pdb ; pdb.set_trace()
 
     # match up OSM & Logainm
     with printer("matching up"):
@@ -145,11 +169,13 @@ def main():
 
         for func in [
                     single_exact_name_and_name_ga_match_and_inside,
-                    single_exact_name_and_optional_name_ga_match_and_inside
+                    single_exact_name_and_optional_name_ga_match_and_inside,
+                    name_and_optional_name_ga_match_very_near,
                 ]:
+            name = func.__name__
             new_logainm_candidates, new_townlands = match_up_pass(townlands, logainms, func)
             logainm_candidates.update(new_logainm_candidates)
-            logger.info("Matched up %d townlands, only %d left", len(townlands) - len(new_townlands), len(new_townlands))
+            logger.info("Matched up %d townlands with %s, only %d left", len(townlands) - len(new_townlands), name, len(new_townlands))
             townlands = new_townlands
 
 
