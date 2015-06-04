d3de85318ac2ddbb638ac2d4c1a05feb9233d37f
Rory McCann <rory@technomancy.org>
2015-06-04 15:36:47 +0200

    scripts

--- update-data.sh
+++ update-data.sh
@@ -4,4 +4,6 @@ wget -O ireland-and-northern-ireland.osm.pbf http://planet.openstreetmap.ie/irel
 ./create-xml-files.sh
 for TYPE in townlands eds civil_parishes baronies counties ; do
     wget -O ${TYPE}.zip http://www.townlands.ie/static/downloads/${TYPE}.zip
+    rm -rf ${TYPE}
+    aunpack ${TYPE}.zip
 done
