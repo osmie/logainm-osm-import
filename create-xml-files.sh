dcc327e2d776c46450a6b381185f278e6bb0995a
Rory McCann <rory@technomancy.org>
2015-06-05 08:53:23 +0200

    save oboth

--- create-xml-files.sh
+++ create-xml-files.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-osmosis --read-pbf ireland-and-northern-ireland.osm.pbf --lp --tag-filter reject-way --tag-filter reject-node --tag-filter accept-relations admin_level=10 --write-xml townlands.osm.xml
+osmosis --read-pbf ireland-and-northern-ireland.osm.pbf --tag-filter reject-way --tag-filter reject-node --tag-filter accept-relations admin_level=* outPipe.0=admin_level --read-pbf ireland-and-northern-ireland.osm.pbf --tag-filter reject-way --tag-filter reject-node --tag-filter accept-relations boundary=* outPipe.0=boundaries --merge inPipe.0=admin_level inPipe.1=boundaries --write-xml boundaries.osm.xml
