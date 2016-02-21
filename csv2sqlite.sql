d98488c7012b12f7aad9f53cf5e52133e36daac3
Rory McCann <rory@technomancy.org>
2016-02-21 21:21:22 +0100

    we should creat indexes

--- csv2sqlite.sql
+++ csv2sqlite.sql
@@ -4,3 +4,8 @@
 .import ./logainm-csvs/geometries.csv geometries
 .import ./logainm-csvs/logainm_categories.csv categories
 create view name_contains as select outer.logainm_id as outer_logainm_id, outer.logainm_category_code as outer_logainm_category_code, outer.name_en as outer_name_en, outer.name_ga as outer_name_ga, inner.logainm_id as inner_logainm_id, inner.logainm_category_code as inner_logainm_category_code, inner.name_en as inner_name_en, inner.name_ga as inner_name_ga from names as outer join geometric_contains as c on (outer.logainm_id = c.outer_obj_id) join names as inner on (inner.logainm_id = c.inner_obj_id);
+create index names__logainm_id on names(logainm_id);
+create index names__name_en on names(name_en);
+create index names__category on names(logainm_category_code);
+create index geometric_contains__outer on geometric_contains(outer_obj_id);
+create index geometric_contains__inner on geometric_contains(inner_obj_id);
