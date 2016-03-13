10a66ce66e341ca8151c915411a3275f93a7384a
Rory McCann <rory@technomancy.org>
2016-03-13 20:09:10 +0100

    docs: Improve logging output

--- match.py
+++ match.py
@@ -191,7 +191,7 @@ def hierachial_matchup(logainm_data, cursor, key, obj_logainm_code, parent_logai
             assert False
 
 
-    logger.info("Matched up %d of %d (%s%%)", len(results), len(possibles), (len(results)*100)/len(possibles))
+    logger.info("Matched up %d %s of %d (%s%%)", len(results), key, len(possibles), (len(results)*100)/len(possibles))
     return results
 
 
